d1eaf7ce403fe7d6cf8e489e978412d77c388600
Mohamed Elrakaiby <mohamed.elrakayby@gmail.com>
2023-05-14 14:37:40 +0200

    Including material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 import { APP_CONFIG } from './config/app.config';
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './modules/app-routing.module';
   ],
   providers: [
     { provide: LOCALE_ID, useValue: APP_CONFIG.locale },
+    { provide: MAT_DATE_LOCALE, useValue: APP_CONFIG.locale },
   ],
   bootstrap: [AppComponent]
 })
